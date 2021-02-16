4974e6de28b69d55360b06ea97d02672e801b474
Arif Ullah <aullah1@hotmail.co.uk>
2021-02-16 21:08:46 +0000

    Remove bundled IIFE JS. Provide both UMD and ES individually.

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,21 @@
+const pkg = require("./package.json");
 import { babel } from '@rollup/plugin-babel';
 import { terser } from "rollup-plugin-terser";
 
+const name = "QuickMenu";
 const config = {
     input: 'src/js/QuickMenu.js',
     output: [
         {
-            file: 'dist/js/quickmenu.bundle.min.js',
-            format: 'iife',
-            name: 'QuickMenu',
+            file: pkg.main,
+            format: 'umd',
+            name: name,
             sourcemap: true,
         },
         {
-            file: 'dist/js/quickmenu.min.js',
+            file: pkg.module,
             format: 'es',
+            name: name,
             sourcemap: true,
         },
     ],
